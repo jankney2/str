6d5895e7b2c6b487e4309630da716b1c952ffbf8
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-3.local>
2020-11-14 12:52:16 -0700

    basic app setup on server

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+
+const app = express();
+require("dotenv").config();
+
+const { PORT, TEST_STR } = process.env;
+const stripe = require("stripe")(TEST_STR);
+
+app.use(express.json())
+
+app.listen(PORT, () => console.log("listening on ", PORT));
+
+app.post("/create-checkout-session", async (req, res) => {
+  const { priceId } = req.body;
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: priceId,
+          quantity: 1
+        }
+      ],
+      // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
+      // the actual Session ID is returned in the query parameter when your customer
+      // is redirected to the success page.
+      success_url:
+        "https://example.com/success.html?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url: "https://example.com/canceled.html"
+    });
+
+    res.send({
+      sessionId: session.id
+    });
+  } catch (e) {
+    res.status(400);
+    return res.send({
+      error: {
+        message: e.message
+      }
+    });
+  }
+});
